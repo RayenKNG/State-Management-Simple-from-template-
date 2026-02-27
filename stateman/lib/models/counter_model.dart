5c40ff19abe4bbffcbee3ff4f5aabca344c3d9cb
Rayen467 <133085728+Rayen467@users.noreply.github.com>
2026-02-27 12:18:09 +0700

    commit: 8 - Membuat CounterModel full dengan ChangeNotifier

--- stateman/lib/models/counter_model.dart
+++ stateman/lib/models/counter_model.dart
@@ -1,30 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-// ChangeNotifier bikin class ini bisa ngasih tau UI kalau ada perubahan data
+// Tambahkan "extends ChangeNotifier" biar class ini bisa jadi Provider
 class CounterModel extends ChangeNotifier {
-  int _angka = 0; // Datanya private
+  // Variabel private (ada garis bawahnya) buat nyimpen data angka
+  int _angka = 0;
 
-  // Cara UI ngambil datanya
+  // 'Getter' buat ngambil nilai _angka supaya bisa dibaca sama UI (tampilan layar)
   int get angka => _angka;
 
-  // Fungsi Tambah
+  // Fungsi buat nambahin angka +1
   void increment() {
     _angka++;
     notifyListeners(); // Wajib ada! Ini yang ngasih tau UI "Woy data berubah, update layarnya!"
   }
 
-  // Jawaban Latihan No 2: Fungsi Kurang
+  // Fungsi buat ngurangin angka -1 (Jawaban Latihan PDF)
   void decrement() {
     _angka--;
-    notifyListeners();
+    notifyListeners(); // Kasih tau UI juga kalau angkanya berkurang
   }
 }
